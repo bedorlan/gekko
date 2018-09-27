a090957ea8b227013a6666f3df0f9c6c32ea48f3
bedorlan <orlando.bedoya@openintl.com>
2018-09-27 17:29:23 -0500

    my first strat

---
+++ strategies/my.js
@@ -0,0 +1,51 @@
+// Source: https://raw.githubusercontent.com/imperator6/gekko/stable/strategies/EMA_OR_PRICE_DIV.js
+// Downloaded from: https://github.com/xFFFFF/Gekko-Strategies
+// helpers
+var _ = require('lodash');
+var log = require('../core/log.js');
+// log.debug = console.log
+
+// configuration
+var config = require('../core/util.js').getConfig();
+var settings = config.my;
+
+// let's create our own method
+var method = {};
+
+// prepare everything our method needs
+method.init = function() {
+  this.name = 'my';
+
+  this.requiredHistory = config.tradingAdvisor.historySize;
+
+  // define the indicators we need
+  this.addIndicator('ema', 'EMA', settings.weight);
+}
+
+// what happens on every new candle?
+method.update = function(candle) {
+  const ema = this.indicators.ema.result
+  this.prev = this.current || ema
+  this.current = ema
+  // log.debug(`${this.indicators.ema.age},${this.indicators.ema.result},${candle.close}`)
+}
+
+// for debugging purposes: log the last calculated
+// EMAs and diff.
+method.log = function() {
+}
+
+method.check = function(candle) {
+  const slope = this.current / this.prev
+  const variation = slope - 1.0
+  log.debug('slope', slope, 'variation', variation)
+
+  if (variation > settings.long) {
+    this.advice('long')
+  
+  } else if (variation < settings.short) {
+    this.advice('short')
+  }
+}
+
+module.exports = method;
