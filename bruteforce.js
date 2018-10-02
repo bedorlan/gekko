31a1f69ca35f1c0459f7577c283ebfd957de314b
bedorlan <orlando.bedoya@openintl.com>
2018-10-02 17:36:53 -0500

    shows time left

--- bruteforce.js
+++ bruteforce.js
@@ -1,39 +1,40 @@
 require('isomorphic-fetch')
 const fs = require('fs')
-const { of, from, Subject } = require('rxjs')
-const { mergeMap, max, tap } = require('rxjs/operators')
+const { from } = require('rxjs')
+const { filter, mergeMap, max, tap } = require('rxjs/operators')
 
 const variables = [
   {
-    name: 'down',
+    name: 'short',
     start: 0,
-    end: -0.1,
-    increment: 0.02,
+    end: -0.0014,
+    increment: 0.0002,
   },
   {
-    name: 'up',
-    start: 0.02,
-    end: 0.12,
-    increment: 0.02,
+    name: 'long',
+    start: 0,
+    end: 0.0014,
+    increment: 0.0002,
   },
   {
     name: 'weight',
-    start: 61,
-    end: 161,
-    increment: 20,
+    start: 1,
+    end: 101,
+    increment: 10,
   },
 ]
 
-// const algorithm = "my"
-// const testTpl = {"watch":{"exchange":"kraken","currency":"USD","asset":"XRP"},"paperTrader":{"feeMaker":0.25,"feeTaker":0.25,"feeUsing":"maker","slippage":0.05,"simulationBalance":{"asset":1,"currency":100},"reportRoundtrips":true,"enabled":true},"tradingAdvisor":{"enabled":true,"method":"my","candleSize":12,"historySize":10},"my":{"weight":25,"long":0.0005,"short":0},"backtest":{"daterange":{"from":"2018-08-05T00:00:00Z","to":"2018-08-17T00:00:00Z"}},"backtestResultExporter":{"enabled":true,"writeToDisk":false,"data":{"stratUpdates":false,"roundtrips":true,"stratCandles":true,"stratCandleProps":["open"],"trades":true}},"performanceAnalyzer":{"riskFreeReturn":2,"enabled":true},"valid":true}
-const algorithm = 'DEMA'
+const algorithm = 'my'
+// prettier-ignore
+const testTpl = {"watch":{"exchange":"poloniex","currency":"USDT","asset":"XRP"},"paperTrader":{"feeMaker":0.25,"feeTaker":0.25,"feeUsing":"maker","slippage":0.05,"simulationBalance":{"asset":1,"currency":100},"reportRoundtrips":true,"enabled":true},"tradingAdvisor":{"enabled":true,"method":"my","candleSize":5,"historySize":288},"my":{"weight":71,"long":0.0012,"short":-0.0014},"backtest":{"daterange":{"from":"2018-09-25T00:00:00Z","to":"2018-10-02T00:00:00Z"}},"backtestResultExporter":{"enabled":true,"writeToDisk":false,"data":{"stratUpdates":false,"roundtrips":true,"stratCandles":true,"stratCandleProps":["open"],"trades":true}},"performanceAnalyzer":{"riskFreeReturn":2,"enabled":true},"valid":true}
+// const algorithm = 'DEMA' // recuerda descomentar la primer linea de la funcion test
 // const testTpl = {"watch":{"exchange":"kraken","currency":"USD","asset":"XRP"},"paperTrader":{"feeMaker":0.25,"feeTaker":0.25,"feeUsing":"maker","slippage":0.05,"simulationBalance":{"asset":1,"currency":100},"reportRoundtrips":true,"enabled":true},"tradingAdvisor":{"enabled":true,"method":"DEMA","candleSize":12,"historySize":10},"DEMA":{"weight":21,"thresholds":{"down":-0.001,"up":0.001}},"backtest":{"daterange":{"from":"2018-08-05T00:00:00Z","to":"2018-08-17T00:00:00Z"}},"backtestResultExporter":{"enabled":true,"writeToDisk":false,"data":{"stratUpdates":false,"roundtrips":true,"stratCandles":true,"stratCandleProps":["open"],"trades":true}},"performanceAnalyzer":{"riskFreeReturn":2,"enabled":true},"valid":true}
 // prettier-ignore
-const testTpl = {"watch":{"exchange":"poloniex","currency":"USDT","asset":"XRP"},"paperTrader":{"feeMaker":0.25,"feeTaker":0.25,"feeUsing":"maker","slippage":0.05,"simulationBalance":{"asset":1,"currency":100},"reportRoundtrips":true,"enabled":true},"tradingAdvisor":{"enabled":true,"method":"DEMA","candleSize":5,"historySize":100},"DEMA":{"weight":21,"thresholds":{"down":0,"up":0.03}},"backtest":{"daterange":{"from":"2018-09-01T00:00:00Z","to":"2018-10-01T00:00:00Z"}},"backtestResultExporter":{"enabled":true,"writeToDisk":false,"data":{"stratUpdates":false,"roundtrips":true,"stratCandles":true,"stratCandleProps":["open"],"trades":true}},"performanceAnalyzer":{"riskFreeReturn":2,"enabled":true},"valid":true}
+// const testTpl = {"watch":{"exchange":"poloniex","currency":"USDT","asset":"XRP"},"paperTrader":{"feeMaker":0.25,"feeTaker":0.25,"feeUsing":"maker","slippage":0.05,"simulationBalance":{"asset":1,"currency":100},"reportRoundtrips":true,"enabled":true},"tradingAdvisor":{"enabled":true,"method":"DEMA","candleSize":5,"historySize":288},"DEMA":{"weight":21,"thresholds":{"down":-0.025,"up":0.025}},"backtest":{"daterange":{"from":"2018-09-24T00:00:00Z","to":"2018-10-02T00:00:00Z"}},"backtestResultExporter":{"enabled":true,"writeToDisk":false,"data":{"stratUpdates":false,"roundtrips":true,"stratCandles":true,"stratCandleProps":["open"],"trades":true}},"performanceAnalyzer":{"riskFreeReturn":2,"enabled":true},"valid":true}
 // const testTpl = {"watch":{"exchange":"binance","currency":"USDT","asset":"XRP"},"paperTrader":{"feeMaker":0.25,"feeTaker":0.25,"feeUsing":"maker","slippage":0.05,"simulationBalance":{"asset":1,"currency":100},"reportRoundtrips":true,"enabled":true},"tradingAdvisor":{"enabled":true,"method":"DEMA","candleSize":12,"historySize":10},"DEMA":{"weight":21,"thresholds":{"down":-0.025,"up":0.025}},"backtest":{"daterange":{"from":"2018-08-05T00:00:00Z","to":"2018-08-17T00:00:00Z"}},"backtestResultExporter":{"enabled":true,"writeToDisk":false,"data":{"stratUpdates":false,"roundtrips":true,"stratCandles":true,"stratCandleProps":["open"],"trades":true}},"performanceAnalyzer":{"riskFreeReturn":2,"enabled":true},"valid":true}
 
 async function test(vars) {
-  vars = { ...vars, thresholds: vars } // for DEMA
+//   vars = { ...vars, thresholds: vars } // for DEMA
   const tpl = { ...testTpl, [algorithm]: vars }
   const response = await fetch('http://localhost:3000/api/backtest', {
     method: 'POST',
@@ -61,9 +62,7 @@ function evalAlphaTrades(result) {
 }
 
 function evalBalanceTrades(result) {
-  return (
-    result.performanceReport.balance * 1000 + result.performanceReport.trades
-  )
+  return result.performanceReport.balance
 }
 
 function evalPreferingRoundtrips(result) {
@@ -79,19 +78,26 @@ function evalPreferingRoundtrips(result) {
 function prettierResult(x) {
   const y = { ...x }
   delete y.result
-  return { ...y, result: x.result.performanceReport }
+  return { ...y, result: { performanceReport: x.result.performanceReport } }
 }
 
 async function main() {
   try {
     const logFile = fs.createWriteStream('bruteforce.csv')
-    const subject = new Subject()
+
+    const tests = []
     const aggregatorFn = variables.reduce(
       variableAggregator,
-      subject.next.bind(subject),
+      tests.push.bind(tests),
     )
 
-    from(subject)
+    aggregatorFn()
+
+    const totalTests = tests.length
+    let testsDone = 0
+    const timeStarted = Date.now()
+
+    from(tests)
       .pipe(
         // mergeMap(test)
         mergeMap(async vars => {
@@ -109,6 +115,18 @@ async function main() {
             // performance: evalAlphaTrades(result)
           }
         }, 4),
+        tap(() => {
+          ++testsDone
+          const timeSpend = Date.now() - timeStarted
+          const testsLeft = totalTests - testsDone
+          const timePerTest = timeSpend / testsDone
+          const timeLeft = timePerTest * testsLeft
+          const millisPerMinute = 1000 * 60
+          const minutesLeft =
+            Math.trunc((timeLeft * 100) / millisPerMinute) / 100
+          console.log('timeLeft', minutesLeft)
+        }),
+        filter(x => x.result.performanceReport.trades >= 2),
         tap(x => {
           const y = prettierResult(x)
           logFile.write(`${y.weight},${y.up},${y.down},${y.performance}\n`)
@@ -128,7 +146,6 @@ async function main() {
       })
 
     aggregatorFn()
-    subject.complete()
   } catch (err) {
     console.error(err)
   }
