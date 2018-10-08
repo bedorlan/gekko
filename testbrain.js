37acfceb0764d35f28f2b05598941be3a6151eb7
bedorlan <orlando.bedoya@openintl.com>
2018-10-08 17:00:13 -0500

    trying tensorflow

--- testbrain.js
+++ testbrain.js
@@ -1,37 +1,79 @@
 const brain = require('brain.js')
+const tf = require('@tensorflow/tfjs')
+require('@tensorflow/tfjs-node')
 const sqlite3 = require('sqlite3')
 const { promisify } = require('util')
 
 const db = new sqlite3.Database('history/poloniex_0.1.db')
 db.all = promisify(db.all)
 
+const groupsQuantity = 101
+
 async function main() {
   try {
-    // const sql = 'select * from candles_USDT_XRP limit 10'
-    // const rows = (await db.all(sql)).map(row => [row.id, row.id % 2])
-    // console.log(rows)
+    // el mes arranca en zero :/
+    const startDate = new Date(2018, 10 - 1, 1).getTime() / 1000
+    const endDate = new Date(2018, 10 - 1, 2).getTime() / 1000
 
-    const rows = [
-      [1, 0, 1, 1, 1, 2],
-      [1, 0, 1, 1, 1, 2],
-      [1, 0, 1, 1, 1, 2],
-      [1, 1, 1, 2, 1, 0],
-      [1, 1, 1, 2, 1, 0],
-      [1, 1, 1, 2, 1, 0],
-      [1, 2, 1, 0, 1, 1],
-      [1, 2, 1, 0, 1, 1],
-      [1, 2, 1, 0, 1, 1],
-    ]
+    const sql = `
+      select *
+      from candles_USDT_XRP
+      where start >= ?
+      and start < ?
+      order by start asc
+    `
+    let result = await db.all(sql, [startDate, endDate])
+    let rows = result.map(r => r.open)
+    rows = normalizeRows(rows)
+    // rows = chunkRows(rows, 60)
 
-    const net = new brain.recurrent.LSTMTimeStep()
-    net.train(rows, { log: true })
+    // const inputShape = tf.util.inferShape(rows)
+    const inputShape = [24, 60]
+    const model = tf.sequential()
+    model.add(
+      tf.layers.lstm({
+        units: groupsQuantity,
+        inputShape,
+        returnSequences: true,
+      }),
+    )
 
-    console.log(net.run([1, 1, 1, 2, 1]))
-    console.log(net.run([1, 2, 1, 0, 1]))
-    console.log(net.run([1, 0, 1, 1, 1]))
+    model.compile({ optimizer: 'sgd', loss: 'meanSquaredError' })
+    const tensor = tf.tensor3d(rows, [1, 24, 60])
+    tensor.print()
+    // await model.fit(tensor)
+
+    // console.log(JSON.stringify(rows))
+
+    // const net = new brain.recurrent.LSTMTimeStep()
+    // net.train(rows, { log: true })
+    // console.log(JSON.stringify(net.toJSON(), null, 2))
   } catch (err) {
     console.error(err)
   }
+}
+
+function normalizeRows(rows) {
+  let uniqueRows = new Set(rows)
+  uniqueRows = Array.from(uniqueRows).sort()
+
+  const groupSize = Math.trunc(uniqueRows.length / groupsQuantity)
+  const groups = Array(groupsQuantity - 1)
+    .fill()
+    .map((value, i) => uniqueRows[(i + 1) * groupSize])
+    .sort()
+  groups.push(Number.MAX_SAFE_INTEGER)
+
+  const groupZero = Math.trunc(groupsQuantity / 2)
+  const normalizedRows = rows.map(r => groups.findIndex(g => r < g) - groupZero)
+
+  return normalizedRows
+}
+
+function chunkRows(rows, size) {
+  return Array(Math.ceil(rows.length / size))
+    .fill()
+    .map((v, i) => rows.slice(i * size, (i + 1) * size))
 }
 
 main()
